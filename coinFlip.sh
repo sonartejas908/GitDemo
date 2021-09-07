61817e0edd20e571b55e05116911e4f1da3b41c1
Tejassonar <sonartejas908@gmail.com>
2021-09-07 16:35:04 +0530

    Running loop 10 times

--- coinFlip.sh
+++ coinFlip.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -x
-A=$((RANDOM%2))
-	if [ $A -eq 0 ]
-	then
-		echo "Tails"
-	elif [ $A -eq 1 ]
-	then
-		echo "Heads"
-	fi
+B=1
+while [ $B != 10 ]
+do
+	A=$((RANDOM%2))
+		if [ $A -eq 0 ]
+		then
+			echo "Tails"
+		elif [ $A -eq 1 ]
+		then
+			echo "Heads"
+		fi
+	((B++))
+done
